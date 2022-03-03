dcc5396c8b1bcf31106e7ed76bedc12b9c0b7e91
mehmoodah <98833732+mehmoodah@users.noreply.github.com>
2022-03-03 11:04:37 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,15 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+require('aframe');
+require('aframe-room-component');
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends Component {
+  render () {
+    return (
+    <a-scene>
+        <rw-room position="-2 0 -2" material="color:#866">
+            <rw-wall position="4 0 0"></rw-wall>
+            <rw-wall position="4 0 4"></rw-wall>
+            <rw-wall position="0 0 4"></rw-wall>
+            <rw-wall position="0 0 0">
+                <rw-doorhole id="holeA"></rw-doorhole>
+                <rw-doorlink from="#holeA" to="#holeB" position="2.5 0 0"></rw-doorlink>
+            </rw-wall>
+        </rw-room>
+        <rw-room position="0 0 -3">
+            <rw-wall position=" 1 0 -1" material="color:#787"></rw-wall>
+            <rw-wall position=" 1 0  1" material="color:#877">
+                <rw-doorhole id="holeB"></rw-doorhole>
+            </rw-wall>
+            <rw-wall position="-1 0  1" material="color:#878"></rw-wall>
+            <rw-wall position="-1 0 -1" material="color:#778"></rw-wall>
+        </rw-room>
+    </a-scene>
+    );
+    }
+  }
