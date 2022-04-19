389701c3dcc5f7aa22bfc9efcc7f1e7dfdff47f5
alialqarni2040 <98093983+alialqarni2040@users.noreply.github.com>
2022-04-19 10:37:29 -0500

    Add files via upload

--- src/components/AFrame.js
+++ src/components/AFrame.js
@@ -5,6 +5,8 @@ import 'https://aframe.io/releases/1.2.0/aframe.min.js';
 import 'https://cdn.rawgit.com/donmccurdy/aframe-extras/v6.1.0/dist/aframe-extras.min.js';
 
 
+
+
 //--------------------IMPORT OBJECTS----------------------//
 
 //
@@ -23,98 +25,301 @@ class AFrameEnviroment extends React.Component{
                     <img id="skyTexture" src="https://cdn.aframe.io/a-painter/images/sky.jpg"/>
                     <audio id="office" src="https://cdn.glitch.global/0e699687-00d8-48d5-9d83-9e74473ca550/office_ambiance.wav?v=1649732899279"></audio>
                     <img id="groundTextures" src="https://images.pexels.com/photos/82256/pexels-photo-82256.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"/>
-
-                    <a-asset-item id="man"
-                                    src="https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ifeoma/Models/business_man/scene.gltf">
-                    </a-asset-item>
-
-
                 </a-assets>
 
-                {/* see for more: https://sketchfab.com/3d-models/business-man-low-polygon-game-character-b6f6740f883b4749abac47af0045a9dd */}
 
-                <a-gltf-model
-                    position="0 0 -3"
-                    scale="1 1 1"
-                    src="#man"
-                    animation-mixer="clip:Rig|sitting_idle"
-                ></a-gltf-model>
-
-                {/*Instruction text*/}
-                <a-entity 
-                    position="-3.000 1.000 -7.500"
-                    scale=".300 .300 .300"
-                    id="instructionText"
-                    text="color: #809dff; align: center; width: 12; wrapCount: 35; zOffset: 0.1; value: Welcome.\nHere are the instructions:\n 1. \n 2. \n 3. \n~" look-at="0 0 0"
-                    geometry="primitive: plane; width: auto; height: auto"
-                    material="color: #ffffff; shader: flat; visible: true">
-                </a-entity>
                 
-                
-                {/*Office background sound
+                {/*Office background sound*/}
                 <a-entity sound="src: url(https://cdn.glitch.global/0e699687-00d8-48d5-9d83-9e74473ca550/office_ambiance.wav?v=1649732899279);
                                 autoplay: true;
                                 refDistance: 4;
                                 rolloffFactor: 4">
-                </a-entity>*/}
+                </a-entity>
 
                 {/*plant*/}
                 <a-entity 
-                        gltf-model="url(https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ifeoma/Models/plant/scene.gltf)"
+                        gltf-model="url(https://cdn.glitch.global/0e699687-00d8-48d5-9d83-9e74473ca550/scene%20(3).glb?v=1649148396664)"
                         scale=".001 .001 .001"
-                        position="3 0 -6"
+                        position="2 0 -7.6"
                 ></a-entity>
+                <a-entity 
+                        gltf-model="url(https://cdn.glitch.global/0e699687-00d8-48d5-9d83-9e74473ca550/scene%20(3).glb?v=1649148396664)"
+                        scale=".001 .001 .001"
+                        position="-7.9 0 -7.6"
+                ></a-entity>
+                 
+                
 
                 {/*drawer*/}
                 <a-entity 
-                        gltf-model="url(https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ifeoma/Models/drawer/model.gltf)"
+                        gltf-model="url(https://cdn.glitch.global/0e699687-00d8-48d5-9d83-9e74473ca550/drawer.glb?v=1649167482215)"
                         scale=".100 .100 .100"
                         position="3 0 -7"
                 ></a-entity>
+                
+                
 
+                
 
+                {/*Josh model*/}
+                
+                
+                <a-asset-item id="man"
+                          src="https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ali/Models/talking_2/scene.gltf">
+                    </a-asset-item>
 
-                 {/*Simple Office Table*/}
-                 <a-entity 
-                        gltf-model="url(https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ifeoma/Models/simple_office_table/model.gltf)"
-                        scale="10 10 10"
-                        position="-2 0 -3"
+                <a-gltf-model
+                    position="-1.5 -.2 -7"
+                    scale="1.1 1.1 1.2"
+                    rotation="0 -10 0"
+                    src="#man"
+                    animation-mixer="clip:Animation"
+                ></a-gltf-model>
+               
+
+                
+        {/*Wood Boss Office Table*/}
+                <a-entity 
+                        gltf-model="url(https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ifeoma/Models/wood_boss_office_chair/model.gltf)"
+                        scale=".006 .006 .006"
+                        position="-1.5 1 -6"
+                        rotation="0 -90 0"
+                        
                 ></a-entity>
+
 
                 {/*Black Office Chair*/}
                 <a-entity 
-                        gltf-model="url(https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ifeoma/Models/Office_Chair/model.gltf)"
+                        gltf-model="url(https://cdn.glitch.global/0e699687-00d8-48d5-9d83-9e74473ca550/OfficeChair.glb?v=1649706093123)"
                         scale="1.5 1.5 1.5"
-                        position="3 0 -3"
+                        position="-1.5 0 -7"
+                        rotation="0 0 0"
                 ></a-entity>
+
+                {/*Board and table*/}
+                <a-entity 
+                        gltf-model="url(https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ali/Models/a_class_room/scene.gltf)"
+                        scale="5 5 5"
+                        position="-6 0 -7"
+                        rotation="0 0 0"
+                ></a-entity>
+
+                
+            {/*folders*/}
+                <a-entity 
+                        gltf-model="url(https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ali/Models/folders/scene.gltf)"
+                        scale="1.2 1 1.2"
+                        position="3 2 -6.5"
+                        rotation="0 90 0"
+                        
+                ></a-entity>
+                <a-entity 
+                        gltf-model="url(https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ali/Models/folders/scene.gltf)"
+                        scale="1.2 1 1.2"
+                        position="3.05 2 -6.5"
+                        rotation="0 90 0"
+                        
+                ></a-entity>
+
+                {/*office_phone*/}
+                <a-entity 
+                        gltf-model="url(https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ali/Models/office_phone/scene.gltf)"
+                        scale=".0001 .0001 .0001"
+                        position="-2.4 1.1 -6"
+                        rotation="0 0 0"
+                        
+                ></a-entity>
+
+                {/*Notebook_and_pen*/}
+                <a-entity 
+                        gltf-model="url(https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ali/Models/notebook_and_pen/scene.gltf)"
+                        scale=".05 .05 .05"
+                        position="-1 1.1 -6"
+                        rotation="0 0 0"
+                        
+                ></a-entity>
+
+            {/*window_blinds*/}
+                <a-entity 
+                        gltf-model="url(https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ali/Models/window_blinds/scene.gltf)"
+                        scale=".02 .03 .02"
+                        position="-2 0.5 -9"
+                        rotation="0 0 0"
+                        
+                ></a-entity>
+
+                <a-entity 
+                        gltf-model="url(https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ali/Models/window_blinds/scene.gltf)"
+                        scale=".02 .03 .02"
+                        position="-6.5 0.5 -9"
+                        rotation="0 0 0"
+                        
+                ></a-entity>
+
+                {/*Office_printer*/}
+                <a-entity 
+                        gltf-model="url(https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ali/Models/mfp_office_printer/scene.gltf)"
+                        scale=".01 .015 .01"
+                        position="1 0 -7.5"
+                        rotation="0 0 0"
+                        
+                ></a-entity>
+
+                {/*table_meeting*/}
+                <a-entity 
+                        gltf-model="url(https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ali/Models/table%20meeting/scene.gltf)"
+                        scale=".03 .03 .03"
+                        position="-5 0 0"
+                        rotation="0 0 0"
+                        
+                ></a-entity>
+
+                {/*coffee_table*/}
+                <a-entity 
+                        gltf-model="url(https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ali/Models/jahre_coffee_table_-_109385/scene.gltf)"
+                        scale=".9 2 .9"
+                        position="-1.4 0 -4.8"
+                        rotation="0 0 0"
+                        
+                ></a-entity>
+
+                {/*low_poly_laptop*/}
+                 <a-entity 
+                        gltf-model="url(https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ali/Models/low_poly_laptop/scene.gltf)"
+                        scale="0.09 0.09 0.09"
+                        position="-1.5 1.1 -6"
+                        rotation="0 180 0"
+                        
+                ></a-entity>
+
+                
 
                 {/*White Office Chair*/}
                 <a-entity 
                         gltf-model="url(https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ifeoma/Models/White_Office_Chair/model.gltf)"
-                        scale="1.5 1.5 1.5"
-                        position="3 0 -2"
-                ></a-entity>
-
-                 {/*Wood Boss Office Table*/}
-                 <a-entity 
-                        gltf-model="url(https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ifeoma/Models/wood_boss_office_chair/model.gltf)"
-                        scale=".005 .005 .005"
-                        position="3 1 0"
-                ></a-entity>
-
-                 {/*robot*/}
-                 {/*
-                 <a-entity 
-                        gltf-model="url(https://raw.githubusercontent.com/ifeomaok/models/main/Models/robot_playground/scene.gltf)"
                         scale="1 1 1"
-                        position="3 0 3"
-                        animation-mixer="clip: Experiment"
+                        position="-2.6 0 -4"
+                        rotation="0 -40 0"
                 ></a-entity>
-                 */}
+                <a-entity 
+                        gltf-model="url(https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ifeoma/Models/White_Office_Chair/model.gltf)"
+                        scale="1 1 1"
+                        position="-0.3 0 -5.5"
+                        rotation="0 -220 0"
+                ></a-entity>
+                
+
+                {/*clock*/}
+                <a-entity 
+                        gltf-model="url(https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ali/Models/clock_3/scene.gltf)"
+                        scale="1.2 1.2 1.2"
+                        position="-2.2 2 -7.9"
+                        rotation="0 0 0"
+                        
+                ></a-entity>
+
+                {/*coffee_machine*/}
+                <a-entity 
+                        gltf-model="url(https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ali/Models/coffee_machine/scene.gltf)"
+                        scale=".5 .5 .5"
+                        position="3. 1.2 -4"
+                        rotation="0 90 0"
+                        
+                ></a-entity>
+
+               {/*coffee_table*/}
+               <a-entity 
+                        gltf-model="url(https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ali/Models/lion_paw_table/scene.gltf)"
+                        scale="15 30 15"
+                        position="3.2 -0.7 -4.2"
+                        rotation="0 180 0"
+                        
+                ></a-entity>
+
+                {/*floor_lamp*/}
+                <a-entity 
+                        gltf-model="url(https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ali/Models/waterloo_floor_lamp_whitebrushed_brass_-_56053/scene.gltf)"
+                        scale="1 1.2 1"
+                        position="-2 0 -7.5"
+                        rotation="0 270 0"
+                        
+                ></a-entity>
+
+                {/*glass_cup*/}
+                <a-entity 
+                        gltf-model="url(https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ali/Models/glass_cup/scene.gltf)"
+                        scale="0.0005 0.0005 0.0005"
+                        position="3. 1.2 -4"
+                        rotation="0 180 0"
+                        
+                ></a-entity>
+                <a-entity 
+                        gltf-model="url(https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ali/Models/glass_cup/scene.gltf)"
+                        scale="0.0005 0.0005 0.0005"
+                        position="3. 1.2 -4.2"
+                        rotation="0 180 0"
+                        
+                ></a-entity>
+
+                {/*floor_lamp*/}
+                <a-entity 
+                        gltf-model="url(https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ali/Models/b2-c/scene.gltf)"
+                        scale="0.05 0.05 0.05"
+                        position="4 1.3 1.3"
+                        rotation="0 -90 0"
+                        
+                ></a-entity>
+
+                <a-entity 
+                        gltf-model="url(https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ali/Models/frame_low_poly/scene.gltf)"
+                        scale="0.02 0.01 0.02"
+                        position="-7.8 .8 1"
+                        rotation="0 -90 0"
+                        
+                ></a-entity>
+
+                {/*simple_door*/}
+                <a-entity 
+                        gltf-model="url(https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ali/Models/simple_door/scene.gltf)"
+                        scale="1 1 1"
+                        position="-2 0 4"
+                        rotation="0 0 0"
+                        
+                ></a-entity>
+
+                 {/*water_fountain*/}
+                 <a-entity 
+                        gltf-model="url(https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ali/Models/water_fountain/scene.gltf)"
+                        scale="0.0015 0.001 0.0015"
+                        position="3.05 1.3 3.9"
+                        rotation="0 180 0"
+                        
+                ></a-entity>
+
+
+                {/*water_glass*/}
+                <a-entity 
+                        gltf-model="url(https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ali/Models/casablanca_water_glass_300ml/scene.gltf)"
+                        scale="0.001 0.0013 0.001"
+                        position="3.15 1.29 3.4"
+                        rotation="0 180 0"
+                        
+                ></a-entity>
+
+                {/*living_room*/}
+                <a-entity 
+                        gltf-model="url(https://raw.githubusercontent.com/mehmoodah/vr-office-proj/Ali/Models/stylised_living_room/scene.gltf)"
+                        scale="1.2 1 1.2"
+                        position="1 0 1"
+                        rotation="0 -90 0"
+                        
+                ></a-entity>
 
 
                 
-                <rw-room position="-2 0 -2" material="color:  #91898c;">
+
+
+                <rw-room position="-2 0 -2" material="color:  #91898c;" height="3">
                     <rw-wall position="6 0 -6"></rw-wall>
                     <rw-wall position="6 0 6"></rw-wall>
                     <rw-wall position="-6 0 6"></rw-wall>
@@ -133,28 +338,13 @@ class AFrameEnviroment extends React.Component{
                 <a-cylinder id="ground" src="#groundTexture" radius="32" height="0.1"/>
 
 
-
-                {/*
-                //Ceiling
-                <a-plane position="0 4 -3" rotation="-270 90 90" width="16" height="15" color="red"/>
-
-                //Left Wall
-                <a-plane position="-8 0 -3" rotation="0 90 0" width="15" height="8" color="green"/>
                 
-                //Right Wall
-                <a-plane position="8 0 -3" rotation="0 -90 0" width="15" height="8" color="green"/>
 
-                //Back Wall
-                <a-plane position="0 0 -10.5" rotation="0 0 0" width="16" height="8" color="blue"/>
+{/* the floor   */}
 
-                //Front Wall
-                <a-plane position="-4 0 4.5" rotation="0 180 0"  width="8" height="8" color="orange"/>
-                <a-plane position="5 0 4.5" rotation="0 180 0"  width="6" height="8" color="orange"/>
-                */}
 
-                {/* the floor   */}
 
-                <a-box src="#groundTextures" width="1" height="0.11" depth="1" position="0 0 0"></a-box> 
+<a-box src="#groundTextures" width="1" height="0.11" depth="1" position="0 0 0"></a-box> 
                 <a-box src="#groundTextures" width="1" height="0.11" depth="1" position="1 0 0"></a-box> 
                 <a-box src="#groundTextures" width="1" height="0.11" depth="1" position="2 0 0"></a-box> 
                 <a-box src="#groundTextures" width="1" height="0.11" depth="1" position="3 0 0"></a-box> 
@@ -361,12 +551,12 @@ class AFrameEnviroment extends React.Component{
 
                 {/* camera */}
 
-                <a-camera position=" 0 1 0" > <a-cursor color="#FF6969" fuse="true"></a-cursor></a-camera>
-
+               
             </a-scene>
 
 
         );
+           
     }
 }
 
